fa2d7c6d04150683fbd16c832bae6d9cd90246a1
Offliner <42777325+Offliners@users.noreply.github.com>
2020-03-22 13:37:03 +0800

    Create goto.c

---
+++ Chapter2/goto.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+        printf("The begin\n");
+        goto end;
+
+        printf("Hello World!\n");
+
+        end :
+        printf("The end\n");
+
+        return 0;
+}

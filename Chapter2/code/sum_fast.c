0693c30f293777acd282d60bc7284532dd8eaa51
Offliner <42777325+Offliners@users.noreply.github.com>
2020-03-23 00:55:20 +0800

    Create sum_fast.c

---
+++ Chapter2/code/sum_fast.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+        int i;
+        int sum;
+        int n;
+
+        scanf("%d", &n);
+        sum = (1 + n) * n / 2;
+
+        printf("The result is %d\n", sum);
+
+        return 0;
+}

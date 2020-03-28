858682b4be1291542cfcc9bef0a6f4726c0e50a6
Offliner <42777325+Offliners@users.noreply.github.com>
2020-03-28 16:50:56 +0800

    Create addr.c

---
+++ Chapter10/code/addr.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+        int a;
+        int b;
+
+        printf("The first address : %p\n", &a);
+        printf("The second address : %p\n", &b);
+
+        return 0;
+}
